e9042c47942902291f63897a5620566af30bd331
Phloraxx <71513365+Phloraxx@users.noreply.github.com>
2022-10-28 21:03:08 +0530

    Update Computer Project.py

--- Computer Project.py
+++ Computer Project.py
@@ -2,14 +2,5 @@ from venv import create
 import mysql.connector as c
 cnx=c.connect(host="localhost",user="root",password="root",database="login")
 crs=cnx.cursor()
-def startup():
-    crs.execute("show tables like 'login'")
-    x=crs.fetchall()
-    for i in x:
-        print(i)
-    if x==[]:
-        print("yes")
-        crs.execute("create table login (Num int(2),User varchar(25),Pass varchar(12))")
-        crs.execute("insert into login values(1,'root','root')")
-startup()
+crs.execute("create table if not exists login (Num int(2),User varchar(25),Pass varchar(12))")
 cnx.commit()
