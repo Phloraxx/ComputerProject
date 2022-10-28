ed37ce202ba6ab677f42f28e9c444ef6ec5b2366
Phloraxx <71513365+Phloraxx@users.noreply.github.com>
2022-10-28 20:45:02 +0530

    Update Computer Project.py

--- Computer Project.py
+++ Computer Project.py
@@ -1,6 +1,15 @@
-#Computer Project
-print(1+2)
-def p(n):
-    print(n+3)
-s=int(input("Enter a number: "))
-p(s)
+from venv import create
+import mysql.connector as c
+cnx=c.connect(host="localhost",user="root",password="root",database="login")
+crs=cnx.cursor()
+def startup():
+    crs.execute("show tables like 'login'")
+    x=crs.fetchall()
+    for i in x:
+        print(i)
+    if x==[]:
+        print("yes")
+        crs.execute("create table login (Num int(2),User varchar(25),Pass varchar(12))")
+        crs.execute("insert into login values(1,'root','root')")
+startup()
+cnx.commit()
