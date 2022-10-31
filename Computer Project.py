0f76b76c51b5d70b4f16599e62b43bc74ddadae2
Phloraxx <71513365+Phloraxx@users.noreply.github.com>
2022-10-31 21:37:12 +0530

    Update Computer Project.py

--- Computer Project.py
+++ Computer Project.py
@@ -2,5 +2,16 @@ from venv import create
 import mysql.connector as c
 cnx=c.connect(host="localhost",user="root",password="root",database="login")
 crs=cnx.cursor()
-crs.execute("create table if not exists login (Num int(2),User varchar(25),Pass varchar(12))")
-cnx.commit()
+def startup():
+    crs.execute("create table if not exists login (Num int(2) primary key,User varchar(25),Pass varchar(12))")
+    crs.execute("insert ignore into  login values(1,'root','root')")
+    cnx.commit()
+startup()
+user=input("Enter your Username: ")
+passs=input("Enter your Password: ")
+crs.execute("select pass from login where user='{}'".format(user))
+check=crs.fetchall()
+for i in check:
+ if i[0]==str(passs):
+    menu()
+   
